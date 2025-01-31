2225cd39ec084b73932fb9cc7ae9e06a63206d75
Risto Heinsar <risto.heinsar@taltech.ee>
2025-01-31 15:20:52 +0200

    Lisatud komponendi liides

---
+++ PrjTekstiPoorajaKomponent/ITeisendaja.vb
@@ -0,0 +1,9 @@
+﻿Public Interface ITeisendaja
+    Property EsimeseTaheAsciiKood As Integer ' Teksti esimese sümboli ASCII kood
+    Property ViimaseTaheAsciiKood As Integer ' Teksti viimase sümboli ASCII kood
+    Property Tekst As String                 ' Kasutaja sisestatud tekst
+
+    Function PooraTekst() As String          ' Liidese meetodid koodi ei oma
+    Sub TeisendaTekst(ByRef sisendTekst As String)
+End Interface
+
